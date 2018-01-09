c59c50ecba02d15d28c4c7fb63af2ec5692bedec
Michael Lanza <30355551+lanzman@users.noreply.github.com>
2018-01-09 13:32:20 -0500

    Update Method Working
    Update Method Working and proper NaN handling along with additional error handling for different data types

--- geturl.py
+++ geturl.py
@@ -1,6 +1,8 @@
 from lxml import html
 import requests
 import pandas as pd
+import os
+
 
 #change directory
 os.chdir(r'C:\Users\mike.lanza\Documents\Python\GitHub\VideoGameAnalytics')
@@ -21,8 +23,8 @@ hltb = requests.get(mainURL)
 #loop through all titles to post requests
 for i in gameslist.loc[gameslist.hltbTitle.isnull()].itertuples():
     
-    #checks if hltbTitle and hltbURL are both not NaN, if they are anything, they have already been processed and can be skipped
-    if ~np.isnan(i.hltbTitle) and ~np.isnan(i.hltbURL):
+    #checks if hltbTitle and hltbURL are both NOT NaN, if they are anything, they have already been processed and can be skipped
+    if (i.hltbTitle == i.hltbTitle) and (i.hltbURL == i.hltbURL):
             
         continue
     
@@ -41,7 +43,7 @@ for i in gameslist.loc[gameslist.hltbTitle.isnull()].itertuples():
     titlepath = '//h3/a/text()'
     titlelist = tree.xpath(titlepath)
     
-    #checks if no list was returned
+    #checks if no list was returned and skips to next if that's true
     if len(titlelist) == 0:
 
         continue
@@ -52,24 +54,40 @@ for i in gameslist.loc[gameslist.hltbTitle.isnull()].itertuples():
     
     #return df for searching
     searchdf = pd.DataFrame(data = {'hltbTitle':titlelist, 'hltbURL':urllist}, index= titlelist)
-    
-    #return dict for mapping hltbURL to hltbTitle
-    #searchdict = dict(zip(titlelist, urllist))
-    
+
+    #using update method and indexing to update gameslist from searchdf for multiple titles if exists
     #map hltbURL to hltbTitle
-    gameslist.update(searchdf)
+    try:
+        gameslist.update(searchdf)
     
-    #set hltbTitle = to Titles for matches
-    #gameslist.hltbTitle = gameslist.loc[gameslist.Titles.isin(searchdf.index)].Titles
-    
-    print(gameslist.loc[i.Index, 'hltbTitle'])
-    
-    if gameslist.loc[i.Index, 'hltbTitle'] == np.NaN and gameslist.loc[i.Index, 'hltbURL'] == np.NaN:
+    #Error handling for issues if multiple games exist with the same title    
+    except ValueError:
+
+        #updates hltbTitle but not the URL since there are duplicate games with the same name and URL needs to be confirmed
+        hltbTitle = titlelist[0]
+
+        #update hltbTitle for current index
+        gameslist.loc[i.Index, 'hltbTitle'] = hltbTitle
         
-        print('test')
+        #move onto next iteration
+        continue
+    
+    try:
+        #If update method did not work because Titles did not match so hltbTitle and hltbURL are still NaN
+        #Take first value from list and update the Title and URL  
+        TitleMatch = not ((gameslist.loc[i.Index, 'hltbTitle'] != gameslist.loc[i.Index, 'hltbTitle']) and (gameslist.loc[i.Index, 'hltbURL'] != gameslist.loc[i.Index, 'hltbURL']))
+    
+    #exception for when there are multiple indexes for the current title all() needs to be used for logic       
+    except ValueError:
+        
+        TitleMatch = not (all(gameslist.loc[i.Index, 'hltbTitle'] != gameslist.loc[i.Index, 'hltbTitle']) and all(gameslist.loc[i.Index, 'hltbURL'] != gameslist.loc[i.Index, 'hltbURL']))
+         
+    
+    if TitleMatch == False:
+        
         #sets the hltb title and url to be the first value
-        htlbTitle = titlelist[0]
-        htlbURL = urllist[0]
+        hltbTitle = titlelist[0]
+        hltbURL = urllist[0]
         
         #updates the values in the list
         gameslist.loc[i.Index, 'hltbTitle'] = hltbTitle
@@ -80,183 +98,13 @@ for i in gameslist.loc[gameslist.hltbTitle.isnull()].itertuples():
 ###
 
 
-
 #reset index
 gameslist = gameslist.reset_index()
 
 #sets titleMatch value
 gameslist.loc[gameslist.hltbTitle == gameslist.Titles, 'titleMatch'] = True
 
-
-
-
-
-
-
-
-
-print(gameslist)
-
-gameslist.hltbURL = searchdf.where(gameslist.Titles.isin(searchdf.index))
-
-gameslist[gameslist.Titles.isin(searchdf.index)]
-
-###This uses the update method and requires matching indexes ie:Title values
-gameslist.update(searchdf)
-test.update(searchdf1)
-
-searchdf = pd.DataFrame(data = {'hltbTitle':titlelist, 'hltbURL':urllist}, index= titlelist)
-
-print(searchdict1)
-gameslist.loc[gameslist.Titles.isin(searchdf1.index)]
-
-print(searchdict)
-gameslist.loc[gameslist.Titles.isin(searchdf.index)]
-
-test = gameslist.set_index('Titles')
-
-test.update(searchdf)
-test.update(searchdf1)
-
-test.hltbTitle = test.loc[test.index.isin(searchdf.index)].index
-
-test.loc[test.index.isin(searchdf.index)].hltbTitle = test.index.where(test.index.isin(searchdf.index))
-
-
-test.loc[test.isin(searchdf)]
-
-test.loc[test.index.isin(searchdf1.index)].hltbTitle
-
-= test.loc[test.index.isin(searchdf.index)].index
-
-gameslist.where(gameslist.Titles.isin(searchdf.index))
-
-searchdf
-searchdf1
-
-#map hltbURL to hltbTitle
-gameslist.loc[gameslist.Titles.isin(searchdf1.index)].hltbURL = gameslist.Titles.map(searchdict1, na_action=None)
-gameslist.loc[gameslist.Titles.isin(searchdf.index)].hltbURL = gameslist.Titles.map(searchdict, na_action=None)
-
-gameslist.hltbURL = gameslist.Titles.map(searchdict1, na_action=None)
-
-
-gameslist.loc[gameslist.Titles.isin(searchdf1.index)]
-gameslist.loc[gameslist.Titles.isin(searchdf.index)]
-
-
-gameslist.hltbURL = gameslist.Titles.map(searchdict, na_action=None)
-
-#set hltbTitle = to Titles for matches
-gameslist.hltbTitle = gameslist.loc[gameslist.Titles.isin(searchdf.hltbTitle)].Titles
-
-
-searchdf = pd.DataFrame(data = {'hltbURL':urllist}, index= titlelist)
-
-print(searchdf)
-
-gameslist.loc[gameslist.Titles.isin(searchdf.index)].hltbURL = searchdf.hltbURL
-
-
-gameslist.loc[gameslist.Titles.isin(searchdf.index)].hltbURL
-
-
-gameslist.hltbURL = gameslist.loc[gameslist.Titles.isin(searchdf.hltbTitle)].Titles
-
-
-
 #writes to csv file
 gameslist.to_csv('GamesList.csv')
 
-
-#reset
-gameslist.loc[:,['hltbTitle', 'hltbURL', 'titleMatch']] = np.NaN
-gameslist.loc[:,['hltbTitle', 'hltbURL', 'titleMatch']]
-
-
-gameslist[:1]
-
-gameslist.loc[gameslist.hltbTitle.isnull()][:5]
-
-#looks through searchdf for other games in list and updates hltbTitle and hltbURL
-for i in gameslist.loc[gameslist.Titles.isin(searchdf.hltbTitle[1:]),:].itertuples():
-
-    gameslist.loc[i.Index,'hltbTitle'] = searchdf.loc[searchdf.hltbTitle == i.Titles]['hltbTitle'].values[0]
-    gameslist.loc[i.Index,'hltbURL'] = searchdf.loc[searchdf.hltbTitle == i.Titles]['hltbURL'].values[0]
-    gameslist.loc[i.Index,'titleMatch'] = True
-
-
-print(searchdf.loc[searchdf.hltbTitle == 'Dead Space 3']['hltbURL'].values)
-
-test = searchdf.loc[searchdf.hltbTitle == 'Dead Space 3']['hltbURL']
-
-
-print(searchdf)
-
-gameslist.loc[gameslist.Titles.isin(searchdf.hltbTitle[1:]),['hltbTitle', 'hltbURL', 'titleMatch']] = ['', '', True]
-
-gameslist.loc[gameslist.Titles == 'Dead Space 3']
-
-
-gameslist.merge(searchdf, on=['hltbTitle', 'hltbURL'])
-
-gameslist.merge(searchdf, on='hltbTitle')
-
-
-#return df for searching
-searchdf = pd.DataFrame({'hltbTitle':titlelist, 'hltbURL':urllist})
-
-gameslist.update(searchdf)
-
-gameslist.loc[gameslist.Titles == 'Dead Space 3']
-
-
-gameslist.loc[gameslist.Titles == 'Dead Space 3'].update(searchdf)
-
-gameslist.mer
-#setup join instead of isin
-
-for i in titlelist:
-        
-    print(i)
-    
-    print(gameslist[gameslist.Titles == i])
-
-
-
-gameslist[:2]
-#title to search
-title = 'Assassin\'s Creed'
-
-#search request using POST
-r = requests.post(searchURL, data = {'queryString' : title})
-
-#get html from page
-tree = html.fromstring(r.content)
-
-#titlelist
-titlepath = '//h3/a/text()'
-titlelist = tree.xpath(titlepath)
-print(titlelist)
-
-#urllist
-urlpath = '//h3/a/@href'
-urllist = tree.xpath(urlpath)
-print(urllist)
-
-#return df for searching
-searchdf = pd.DataFrame({'title':titlelist, 'url':urllist})
-
-#use loop to find match of title in df to get url
-titleurl = searchdf.loc[searchdf['title'] == title]['url'][0]
-
-print(titleurl)
-
-print(titleurl)
-
-hltb.raise_for_status()
-
-hltb.text()
-
-path = '//*[@id="global_search_content"]/li[1]/div[2]/h3/a'
 
