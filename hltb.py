ac4db9e0d063f3bb7da324a115baabbb74b05d5f
Michael Lanza <30355551+lanzman@users.noreply.github.com>
2018-05-29 10:43:57 -0400

    Working Download, Upload, and searching for HLTB
    All functionality working revolving around HLTB and the file download/upload to the Google Drive

--- hltb.py
+++ hltb.py
@@ -2,6 +2,82 @@ from lxml import html
 import requests
 import pandas as pd
 
+
+#performs a manual search of the dataframe
+def manualsearch(gamesdf):
+    
+    #sets the searchURL
+    searchURL = r'https://howlongtobeat.com/search_main.php?page=1'
+        
+    #init yes/no choice    
+    yes = ['yes','y', 'ye', '']
+    no = ['no','n']
+    
+    
+    for i in gamesdf.loc[gamesdf.hltbTitle != gamesdf.hltbTitle].itertuples():
+        
+        #input to enter a title to search for
+        searchtitle = input('Please enter new title for ' + i.Titles + '.\n')
+        
+        #exits loop
+        if searchtitle == 'exit':
+            
+            print('Ending search loop.')
+            break
+        elif searchtitle in no:
+            print('Skipping to next title.\n')
+            continue
+        
+        #while loop to run for continuous searching, will exit on exit
+        while searchtitle != None:
+            
+            #gets the searchdf back from the title search
+            searchdf = titlesearch(searchtitle,searchURL)
+            
+            #checks if there are results
+            if len(searchdf) == 0:
+                
+                print('No results found!')
+                searchresult = 'no'
+            
+            else:
+                #returns the hltbTitle from the search
+                searchresult = input('Resulting title is ' + searchdf.hltbTitle[0] + ', do you accept?\n').lower()
+                
+            #if user accepts search result, the gamesdf will be updated
+            if searchresult in yes:
+    
+                #updates the title in the gamesdf
+                gamesdf.loc[i.Index, 'Titles'] = searchdf.hltbTitle[0]
+                gamesdf.loc[i.Index, 'hltbTitle'] = searchdf.hltbTitle[0]
+                gamesdf.loc[i.Index, 'TitleMatch'] = searchdf.TitleMatch[0]
+                gamesdf.loc[i.Index, 'hltbURL'] = searchdf.hltbURL[0]
+                gamesdf.loc[i.Index, 'mainlength'] = getmainlength(searchdf.hltbURL[0])
+                
+                #for breaking out of searchtitle loop
+                searchtitle = None
+            
+            #if user doesn't accept search result, allows them to search again
+            elif searchresult in no:
+                
+                #user can search again
+                searchtitle = input('Search returned nothing, please enter new titlesearch for ' + i.Titles + '.\n')
+    
+                #if anything else, moves onto next title
+                if searchtitle.lower() in no:
+                    
+                    print('Moving on to next title.')
+                    
+                    #for breaking out of searchtitle loop
+                    searchtitle = None
+    
+            #handles any other answer
+            else:
+                
+                print('Please enter valid answer.')
+
+    return gamesdf
+
 #gets the hltbURL, hltbTitle, and playtime
 def geturl(gamesdf):
     
@@ -26,35 +102,16 @@ def geturl(gamesdf):
             
         if Processed == False:
             #sets the title variable
+            
             title = i.Index
             
-            #search request using POST
-            r = requests.post(searchURL, data = {'queryString' : title})
-        
-            #get html from page
-            tree = html.fromstring(r.content)
+            #perform title search
+            searchdf = titlesearch(title, searchURL)
             
-            #titlelist
-            titlepath = '//h3/a/text()'
-            titlelist = tree.xpath(titlepath)
-            
-            #checks if no list was returned and skips to next if that's true
-            if len(titlelist) == 0:
-        
+            #if titlesearch yields nothing, continue to next item
+            if len(searchdf) == 0:
+                
                 continue
-            
-            #urllist
-            urlpath = '//h3/a/@href'
-            urllist = tree.xpath(urlpath)
-            
-            #create df for searching and updating
-            searchdf = pd.DataFrame(data = {'hltbTitle':titlelist, 'hltbURL':urllist}, index= titlelist)
-            
-            #fills out remaining part of url
-            searchdf['hltbURL'] = 'https://howlongtobeat.com/' + searchdf['hltbURL']
-            
-            #Sets TitleMatch value for searchdf
-            searchdf['TitleMatch'] = True
             
             #using update method and indexing to update gamesdf from searchdf for multiple titles that were returned from search if exists
             #map hltbURL, hltbTitle, and TitleMatch
@@ -76,8 +133,8 @@ def geturl(gamesdf):
         if TitleMatch == False:
             
             #sets the hltb title and url to be the first value
-            hltbTitle = titlelist[0]
-            hltbURL = 'https://howlongtobeat.com/' + urllist[0]
+            hltbTitle = searchdf.hltbTitle[0]
+            hltbURL = searchdf.hltbURL[0]
             
             #updates the values in the list
             gamesdf.loc[i.Index, 'hltbTitle'] = hltbTitle
@@ -97,51 +154,142 @@ def geturl(gamesdf):
                 #sets the searchURL
                 searchURL = gamesdf.loc[i.Index, 'hltbURL']
             
-            #initiates gamepage requests
-            gamepage = requests.get(searchURL)
-               
-            #get html from page
-            tree = html.fromstring(gamepage.content)
-                
-            #tablepath
-            tablepath = '//table[@class="game_main_table"]/tbody[@class="spreadsheet"][1]/tr/td/text()'
-            lengthlist = tree.xpath(tablepath)
-            
-            #if nothing returned, skip to next title
-            if len(lengthlist) == 0:
-                
-                continue
-            
             #sets the mainlength variable
-            mainlength = lengthlist[2]
+            mainlength = getmainlength(searchURL)
         
         #updates the value in the gamesdf
         gamesdf.loc[i.Index,'mainlength'] = mainlength
         
-        print(i.hltbTitle, ' ', mainlength)
+        print(mainlength)
         
     #reset index
     gamesdf.reset_index(level = 0, inplace = True)
     
-#    #sets titleMatch value
-#    gamesdf.loc[gamesdf.hltbTitle == gamesdf.Titles, 'TitleMatch'] = True
-#    gamesdf.loc[gamesdf.hltbTitle != gamesdf.Titles, 'TitleMatch'] = False
-    
-#    #Remove NaNs and replace with URL missing and Title missing
-#    gamesdf.hltbURL = gamesdf.hltbURL.fillna('URL missing')
-#    gamesdf.hltbTitle = gamesdf.hltbTitle.fillna('Title missing')
-    
-#    #sets full URL values
-#    gamesdf.loc[~gamesdf.hltbURL.str.contains('https://howlongtobeat.com/|URL missing') , 'hltbURL'] = 'https://howlongtobeat.com/' + gamesdf.hltbURL 
-    
     #create dataframe with missing data
     checklist = gamesdf.loc[(gamesdf.mainlength == 'URL missing') | (gamesdf.hltbTitle == 'Title missing')].copy()
     
-    ### run last line of code after manually updating Titles
-    ### run urlloop after updating Titles
-    
-#    #update gamesdf with checklist
-#    gamesdf.loc[checklist.index, 'hltbTitle'] = checklist.hltbTitle
-#    gamesdf.loc[checklist.index, 'hltbURL'] = checklist.hltbURL
+    return gamesdf, checklist
 
+#performs titlesearch on hltb site from title that is passed through
+def titlesearch(title, searchURL):
+    
+    #search request using POST
+    r = requests.post(searchURL, data = {'queryString' : title})
+    
+    print('Check that this actually')
+    
+    r.encoding = 'utf-8'
+    
+    #get html from page
+    tree = html.fromstring(r.text)
+    
+    #titlelist
+    titlepath = '//h3/a/text()'
+    titlelist = tree.xpath(titlepath)
+    
+    #checks if no list was returned and skips to next if that's true
+    if len(titlelist) == 0:
+        
+        #creates empty dataframe
+        searchdf = pd.DataFrame()
+        
+        return searchdf
+    
+    #urllist
+    urlpath = '//h3/a/@href'
+    urllist = tree.xpath(urlpath)
+    
+    
+    ####Mess around with the encoding at this point for the searchdf stuff
+    ##might be the part I removed that I shouldn't have or something to do with varying encoding.  for some reason new versions aren't taking either
+    #create df for searching and updating
+    searchdf = pd.DataFrame(data = {'hltbTitle':titlelist, 'hltbURL':urllist}, index= titlelist)
+    
+    #fills out remaining part of url
+    searchdf['hltbURL'] = 'https://howlongtobeat.com/' + searchdf['hltbURL']
+    
+    #Sets TitleMatch value for searchdf
+    searchdf['TitleMatch'] = True
+    
+    #drops the duplicates based on the hltbTitle
+    searchdf.drop_duplicates(subset = ['hltbTitle'], inplace = True)
+    
+    return searchdf
+    
+#gets the mainlength of a game from the URL
+def getmainlength(searchURL):
+    
+    #initiates gamepage requests
+    gamepage = requests.get(searchURL)
+       
+    #get html from page
+    tree = html.fromstring(gamepage.content)
+        
+    #tablepath
+    tablepath = '//table[@class="game_main_table"]/tbody[@class="spreadsheet"][1]/tr/td/text()'
+    lengthlist = tree.xpath(tablepath)
+    
+    #if nothing returned, skip to next title
+    if len(lengthlist) == 0:
+        
+        mainlength = 'URL missing'
+    else:
+        #sets the mainlength variable
+        mainlength = lengthlist[2]
+
+    return mainlength
+
+#checks the collected title vs the entered title and sets the mainlength if accepted
+def verifychecklist(gamesdf, checklist):
+    
+    #init yes/no choice    
+    yes = ['yes','y', 'ye', '']
+    no = ['no','n']
+    
+    #loops through the checklist to allow user to accept new title and get mainlength value
+    for i in checklist.loc[checklist.mainlength == 'URL missing'].itertuples():
+        
+        #prints out the Original Title and HLTB Title
+        print(i.Titles, ' --> ', i.hltbTitle)
+        
+        #sets choice to none
+        choice = None
+        
+        #checks the result of the choice to continue running loop
+        while choice not in yes and choice not in no:
+            
+            #creates yes no input to accept the new title
+            choice = input('Do you accept the new HLTB Title?\n').lower()
+            
+            #if user accepts, runs function to get the mainlength value
+            if choice in yes:
+                
+                #gets mainlength value and sets it in the dataframe
+                checklist.loc[i.Index, 'mainlength'] = getmainlength(i.hltbURL)
+                print('Updated!\n')
+                
+            #if user declines, does nothing
+            elif choice in no:
+                print('Not Updated!\n')
+            
+            #exit allows user to exit the loop
+            elif choice == 'exit':
+                print('Exiting loop!\n')
+                break
+            #if selection not accepted, re-runs loop
+            else:
+                print('Please give a better answer.')
+        
+        #breaks out of loop when exit is selected
+        if choice == 'exit':
+            break
+    
+    #updates the gamesdf
+    gamesdf.update(checklist)
+    
+    #reduces the checklist
+    checklist = checklist.loc[checklist.mainlength == 'URL missing']
+    
+    print('Verification Complete!')
+    
     return gamesdf, checklist
--- main.py
+++ main.py
@@ -5,20 +5,13 @@ Created on Wed May  2 13:48:37 2018
 @author: mike.lanza
 """
 
-#import sys
-#
-##focus on the files directory
-#sys.argv[0]
-#
-
 from GoogleDrive import GDrive
 import hltb
 
-
-#
-#
 #GamesList file ID from GoogleDrive
 GamesListFileID = '1OewpCqeugcbNEZQmsYaB9ItOnV9Kkfvuk83BRJaUHFY'
+GamesListFileName = GDrive.getFilename(GamesListFileID)
+GamesListFilePath = GamesListFileName + '.csv'
 
 #downloads gameslist and converts to a dataframe
 gamesdf = GDrive.DataFrameDownload(GamesListFileID)
@@ -26,6 +19,16 @@ gamesdf = GDrive.DataFrameDownload(GamesListFileID)
 #runs through the hltb geturl function and updates dataframe
 gamesdf, checklist = hltb.geturl(gamesdf)
 
+#passes gamesdf and checklist to manually accept titles, update URLs, and get mainlengths
+gamesdf, checklist = hltb.verifychecklist(gamesdf, checklist)
 
+#passes gamesdf and allows user to manually search for titles
+gamesdf = hltb.manualsearch(gamesdf)
 
-# need to work through this to figure out the proper folder structure to be used all over
+#gets metadata from Games List for updating
+gamesdfmetadata = GDrive.searchFile(10, query = ("name contains '" + GamesListFileName + "'"))
+
+#Updates the Games List File on the google drive
+#will not work with pokemon special characters
+GDrive.df_to_GoogleSheetUpdate(gamesdf, GamesListFileName, gamesdfmetadata[0]['id'])
+
